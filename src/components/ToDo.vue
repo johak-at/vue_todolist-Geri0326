725aa7198ed5593efadd4877d17bb7024898cafe
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2022-10-18 10:56:37 +0200

    Correcting errors and added an extra Class for the ToDoList

---
+++ src/components/ToDo.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(["name", "id"]);
+const emit = defineEmits(["remove"]);
+</script>
+
+<template>
+    <li>
+    {{name}} &nbsp
+    <button @click="emit('remove',  id )">x</button>
+    </li> <br>
+</template>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,49 +1,53 @@
 <script setup>
-    import { ref } from "vue";
-    import { computed } from "vue";
-    const list = ref([]);
-    const newItem = ref("");
-    const filter = ref("");
-    const filteredList = computed(() => {
-        let result = [];
-        for (let i = 0; i < list.value.length; i++) {
-            if (list.value[i].name.toLowerCase().includes(filter.value.toLowerCase())) {
-                result.push(list.value[i]);
-            }
+import { ref } from "vue";
+import { computed } from "vue";
+import ToDo from './ToDo.vue';
+const list = ref([]);
+const newItem = ref("");
+const filter = ref("");
+const filteredList = computed(() => {
+    let result = [];
+    for (let i = 0; i < list.value.length; i++) {
+        if (list.value[i].name.toLowerCase().includes(filter.value.toLowerCase())) {
+            result.push(list.value[i]);
         }
-        return result;
-    });
-    function add() {
-        list.value.push({
-            id: Date.now().toString(16),
-            name: newItem.value,
-        });
-        newItem.value = "";
     }
-    function remove(id) {
+    return result;
+});
+function add() {
+    list.value.push({
+        id: Date.now().toString(16),
+        name: newItem.value,
+    });
+    newItem.value = "";
+}
+function remove(id) {
         for(let i = 0; i < list.value.length; i++) {
             if (list.value[i].id === id) {
                 list.value.splice(i, 1);
             }
                 
             }
+            console.log("test")
         }
+
+function test() {
+    console.log("test");
     
-    </script>
-    
-    <template>
-        <div>
-            <h1>TodoList</h1>
-            <input @keyup.enter="add" v-model="newItem" />
-            <br /><br />
-            <input type="text" placeholder="filter list" v-model="filter" />
-            <ul>
-                <li v-for="item in filteredList" :key="item.id">
-                    {{item.name}}
-                    <button @click="remove(item.id)">x</button>
-                </li>
-            </ul>
-    
-    
-        </div>
-    </template>
+}
+</script>
+
+<template>
+    <div>
+        <h1>ToDoList</h1>
+        <input @keyup.enter="add" v-model="newItem" />
+        <br /><br />
+        <input type="text" placeholder="filter list" v-model="filter" />
+        <ul>
+            <!--use ToDo.vue to add remove button-->
+            <ToDo v-for="item in filteredList" :key="item.id" :name="item.name" :id="item.id" @remove="remove" />
+        </ul>
+
+
+    </div>
+</template>
